c5eba48f619485ad50725ef0edebd45c9c8edf2a
rrosztoczy <rrosztoczy@gmail.com>
2019-03-11 19:36:56 -0400

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,47 @@
 require './environment'
 
+
+# rspec ./spec/01_sinatra_forms_spec.rb:7 # FormsLab::App GET '/' returns a 200 status code
+# rspec ./spec/01_sinatra_forms_spec.rb:11 # FormsLab::App GET '/' renders the instructions
+# rspec ./spec/01_sinatra_forms_spec.rb:22 # FormsLab::App GET '/new' returns a 200 status code
+# rspec ./spec/01_sinatra_forms_spec.rb:26 # FormsLab::App GET '/new' renders a new form element on the page
+# rspec ./spec/01_sinatra_forms_spec.rb:31 # FormsLab::App GET '/new' renders the pirate input fields for name, weight, and height attributes on the page
+# rspec ./spec/01_sinatra_forms_spec.rb:37 # FormsLab::App GET '/new' renders the first ship's input fields for the name, type, and booty attributes on the page
+# rspec ./spec/01_sinatra_forms_spec.rb:43 # FormsLab::App GET '/new' renders the second ship's input field for the name, type, and booty attributes on the page
+# rspec ./spec/01_sinatra_forms_spec.rb:77 # FormsLab::App POST '/pirates' returns a 200 status code
+# rspec ./spec/01_sinatra_forms_spec.rb:81 # FormsLab::App POST '/pirates' displays the pirate information upon form submission
+# rspec ./spec/01_sinatra_forms_spec.rb:87 # FormsLab::App POST '/pirates' displays the first ship's information upon form submission
+# rspec ./spec/01_sinatra_forms_spec.rb:93 # FormsLab::App POST '/pirates' displays the second ship's information upon form submission
+# rspec ./spec/02_interaction_spec.rb:6 # Filling in the Form can fill in forms for a pirate and two ships
+
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do 
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      # puts @pirate.inspect
+      # puts @ships.inspect
+
+      # @ship1 = Ship.new(params[:pirate])
+      # @ship2 = Ship.new(params[:pirate])
+
+      erb :'/pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,27 @@
 class Pirate
+attr_accessor :name, :weight, :height
+@@all = []
+
+def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@all << self
 end
+
+def self.all
+    @@all
+end
+
+
+
+end
+# Create two classes, a Pirate class and a Ship class. Pirates should have a name, weight, and height.
+#  You will also need a class method that returns all the pirates. 
+
+# Make a nested form (this should probably have html <label>s so it makes sense to a user). This form will be creating three objects 
+# (one instance of the Pirate class, and two instances of the Ship class). Remember, you'll need to build a corresponding controller action
+#  to load this page. Your form should POST to the route '/pirates'.
+
+# After a user clicks submit they should be taken to a page that displays all the information we just posted from the form. You should have a page 
+# that shows the pirate you created along with their ships and all the details about the pirate and their ships.
